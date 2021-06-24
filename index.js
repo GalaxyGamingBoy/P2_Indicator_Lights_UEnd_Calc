a21a5ff50ea53b71a0445186ec88313b1bca57bc
GalaxyGamingBoy <xrteach@gmail.com>
2021-06-24 12:04:38 +0000

    Init3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var px = document.getElementById("px");
 var r = document.getElementById("r");
 var px_i = 0;
 submit_button.addEventListener("click", function () {
-    px_i = Number.parseFloat(px.value);
+    px_i = +px.value;
     var i = px_i / 16;
     var ii = i * 0.25;
     r.innerHTML = ii.toString();
--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ const r = document.getElementById("r")
 var px_i: number = 0;
 
 submit_button.addEventListener("click", () => {
-    px_i = Number.parseFloat(px.value)
+    px_i = +px.value
     var i: number = px_i / 16
     var ii: number = i * 0.25
     r.innerHTML = ii.toString()
