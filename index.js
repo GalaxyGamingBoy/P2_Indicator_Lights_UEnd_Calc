95d1ed6aab4f730c0ba7a2ec76d740740785742d
GalaxyGamingBoy <xrteach@gmail.com>
2021-06-24 12:02:43 +0000

    Init 2

---
+++ index.js
@@ -0,0 +1,10 @@
+var submit_button = document.getElementById("submit");
+var px = document.getElementById("px");
+var r = document.getElementById("r");
+var px_i = 0;
+submit_button.addEventListener("click", function () {
+    px_i = Number.parseFloat(px.value);
+    var i = px_i / 16;
+    var ii = i * 0.25;
+    r.innerHTML = ii.toString();
+});
---
+++ index.ts
@@ -0,0 +1,12 @@
+const submit_button = document.getElementById("submit") as HTMLButtonElement;
+const px = document.getElementById("px") as HTMLInputElement;
+const r = document.getElementById("r")
+
+var px_i: number = 0;
+
+submit_button.addEventListener("click", () => {
+    px_i = Number.parseFloat(px.value)
+    var i: number = px_i / 16
+    var ii: number = i * 0.25
+    r.innerHTML = ii.toString()
+})
